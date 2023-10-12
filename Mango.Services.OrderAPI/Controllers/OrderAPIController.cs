f10b98587c684afcd9d1ed8273e493301e981932
tachaow <c_tongtip@hotmail.com>
2023-10-12 22:55:16 +0700

    m commit

--- Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
+++ Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -7,6 +7,9 @@ using Mango.Services.OrderAPI.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.Checkout;
+using System.Security.Policy;
 
 namespace Mango.Services.OrderAPI.Controllers
 {
@@ -53,6 +56,38 @@ namespace Mango.Services.OrderAPI.Controllers
                 _response.Message = ex.Message;
             }
 
+            return _response;
+        }
+
+        [Authorize]
+        [HttpPost("CreateStripeSession")]
+        public async Task<ResponseDto> CreateStripeSession([FromBody] StripeRequestDto stripeRequestDto)
+        {
+            try
+            {
+                StripeConfiguration.ApiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+
+                var options = new SessionCreateOptions
+                {
+                    SuccessUrl = "https://example.com/success",
+                    LineItems = new List<SessionLineItemOptions>
+                  {
+                    new SessionLineItemOptions
+                    {
+                      Price = "price_H5ggYwtDq4fbrJ",
+                      Quantity = 2,
+                    },
+                  },
+                    Mode = "payment",
+                };
+                var service = new SessionService();
+                service.Create(options);
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess= false;
+            }
             return _response;
         }
     }
