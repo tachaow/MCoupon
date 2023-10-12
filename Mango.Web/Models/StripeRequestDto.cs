5a9e7dea61ac007f2b65593da0fc9f4902ac6075
tachaow <c_tongtip@hotmail.com>
2023-10-12 14:43:08 +0700

    m commit

---
+++ Mango.Web/Models/StripeRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Web.Models;
+
+namespace MCoupon.Web.Models
+{
+    public class StripeRequestDto
+    {
+        public string StripeSessionUrl { get; set; }
+        public string StripeSessinId { get; set; }
+        public string ApproveUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public OrderHeaderDto OrderHeader { get; set; }
+    }
+}
