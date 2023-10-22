2d39201ab9781e8b5aaef3846e695a2ccbdb78e1
M Tongtip <c_tongtip@hotmail.com>
2023-10-23 02:07:45 +0700

    150 Add Endpoints to Order Service

--- Mango.Web/Services/IServices/IOrderService.cs
+++ Mango.Web/Services/IServices/IOrderService.cs
@@ -5,8 +5,11 @@ namespace Mango.Web.Services.IServices
 {
     public interface IOrderService
     {
-        Task<ResponseDto> CreateOrder(CartDto cartDto);
-        Task<ResponseDto> CreateStripeSession(StripeRequestDto stripeRequestDto);
-        Task<ResponseDto> ValidateStripeSession(int orderHeaderId);
+        Task<ResponseDto?> CreateOrder(CartDto cartDto);
+        Task<ResponseDto?> CreateStripeSession(StripeRequestDto stripeRequestDto);
+        Task<ResponseDto?> ValidateStripeSession(int orderHeaderId);
+        Task<ResponseDto?> GetAllOrder(string? userId);
+        Task<ResponseDto?> GetOrder(int orderId);
+        Task<ResponseDto?> UpdateOrderStatus(int orderId, string newStatus);
     }
 }
