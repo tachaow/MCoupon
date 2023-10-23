e3f3fbb95a68debfde56d01207c33356b55dd128
M Tongtip <c_tongtip@hotmail.com>
2023-10-23 11:43:00 +0700

    151 Order List UI

---
+++ Mango.Web/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MCoupon.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult OrderIndex()
+        {
+            return View();
+        }
+    }
+}
