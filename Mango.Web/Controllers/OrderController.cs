3baa0833cb376ac09d75064f26bac01b04245238
M Tongtip <c_tongtip@hotmail.com>
2023-10-23 14:08:59 +0700

    153 Load Datatables 1

--- Mango.Web/Controllers/OrderController.cs
+++ Mango.Web/Controllers/OrderController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Mango.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace MCoupon.Web.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
         public IActionResult OrderIndex()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeaderDto> list;
+            string userId = "";
+            if (!User.IsInRole(SD.RoleAdmin))
+            {
+                userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            }
+            ResponseDto response = _orderService.GetAllOrder(userId).GetAwaiter().GetResult();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                list = new List<OrderHeaderDto>();
+            }
+            return Json(new { data = list });
         }
     }
 }
